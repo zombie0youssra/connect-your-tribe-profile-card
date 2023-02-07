e6c2b49ca3160cf9aa1053b864b73267b4e992cd
youssra <112857270+zombie0youssra@users.noreply.github.com>
2023-02-07 13:58:57 +0100

    :anguished:

--- index.js
+++ index.js
@@ -1,27 +1,43 @@
 // Importeer express uit de node_modules map
-import express from 'express'
+import express from "express";
 
 // Maak een nieuwe express app aan
-const app = express()
+const app = express();
 
 // Stel ejs in als template engine en geef de 'views' map door
-app.set('view engine', 'ejs')
-app.set('views', './views')
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
 // Gebruik de map 'public' voor statische resources
-app.use(express.static('public'))
+app.use(express.static("public"));
 
 // Maak een route voor de index
-app.get('/', function (req, res) {
+app.get("/", function (req, res) {
   // res.send('Hello World!')
-  res.render('index')
-})
+  res.render("index");
+});
 
 // Stel het poortnummer in waar express op gaat luisteren
-app.set('port', process.env.PORT || 8000)
+app.set("port", process.env.PORT || 8000);
 
 // Start express op, haal het ingestelde poortnummer op
-app.listen(app.get('port'), function () {
+app.listen(app.get("port"), function () {
   // Toon een bericht in de console en geef het poortnummer door
-  console.log(`Application started on http://localhost:${app.get('port')}`)
-})
+  console.log(`Application started on http://localhost:${app.get("port")}`);
+});
+
+// mijn script
+// scrol animatie voor text
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      // entry.target.classList.remove("show");
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll(".text");
+hiddenElements.forEach((el) => observer.observe(el));
