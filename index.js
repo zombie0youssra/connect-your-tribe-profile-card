1759519e5d934bde914996120f3f28b88192ea72
youssra <112857270+zombie0youssra@users.noreply.github.com>
2023-02-07 15:11:17 +0100

    js werkt nier

--- index.js
+++ index.js
@@ -28,16 +28,17 @@ app.listen(app.get("port"), function () {
 
 // mijn script
 // scrol animatie voor text
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    console.log(entry);
-    if (entry.isIntersecting) {
-      entry.target.classList.add("show");
-    } else {
-      // entry.target.classList.remove("show");
-    }
-  });
-});
 
-const hiddenElements = document.querySelectorAll(".text");
-hiddenElements.forEach((el) => observer.observe(el));
+// const observer = new IntersectionObserver((entries) => {
+//   entries.forEach((entry) => {
+//     console.log(entry);
+//     if (entry.isIntersecting) {
+//       entry.target.classList.add("show");
+//     } else {
+//       entry.target.classList.remove("show");
+//     }
+//   });
+// });
+
+// const hiddenElements = document.querySelectorAll(".text");
+// hiddenElements.forEach((el) => observer.observe(el));
