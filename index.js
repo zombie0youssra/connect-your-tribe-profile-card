be9a1200f24568befed27cbaca7f68947bd7121b
youssra <112857270+zombie0youssra@users.noreply.github.com>
2023-02-08 13:10:57 +0100

    new style

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 // Importeer express uit de node_modules map
-import express from "express";
+import express, { response } from "express";
 
 // Maak een nieuwe express app aan
 const app = express();
+
+// get info form api
+
+// const url=
+// const data= await fetch (url).then((response)=> response.json()
 
 // Stel ejs in als template engine en geef de 'views' map door
 app.set("view engine", "ejs");
@@ -25,20 +30,3 @@ app.listen(app.get("port"), function () {
   // Toon een bericht in de console en geef het poortnummer door
   console.log(`Application started on http://localhost:${app.get("port")}`);
 });
-
-// mijn script
-// scrol animatie voor text
-
-// const observer = new IntersectionObserver((entries) => {
-//   entries.forEach((entry) => {
-//     console.log(entry);
-//     if (entry.isIntersecting) {
-//       entry.target.classList.add("show");
-//     } else {
-//       entry.target.classList.remove("show");
-//     }
-//   });
-// });
-
-// const hiddenElements = document.querySelectorAll(".text");
-// hiddenElements.forEach((el) => observer.observe(el));
